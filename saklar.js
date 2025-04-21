da6a6d6c549a512ac69e010f41adc4c8335a688e
satryo921 <satryotio00@gmail.com>
2025-04-21 08:21:40 +0700

    Add files via upload

--- saklar.js
+++ saklar.js
@@ -1,10 +1,33 @@
-function saklar(params) {
-    let lampu1 = document.getElementById("lampu1");
+function saklar() {
 
-    if (params == "on") {
-        lampu1.src = "assets/images/lampu_on.png";
+
+
+    let toggle1 = document.getElementById("default-toggle1");
+    let toggle2 = document.getElementById("default-toggle2");
+    let toggle3 = document.getElementById("default-toggle3");
+
+    let lampu1 = document.getElementById("lampu1");
+    let lampu2 = document.getElementById("lampu2");
+    let lampu3 = document.getElementById("lampu3");
+    
+    if(toggle1.checked) {
+        lampu1.src = "assets/images/lampu_on.png" 
+    }else {
+        lampu1.src = "assets/images/lampu_off.png"
     }
-    if (params == "off") {
-        lampu1.src = "assets/images/lampu_off.png";
+
+    if(toggle2.checked) {
+        lampu2.src = "assets/images/lampu_on.png" 
+    }else {
+        lampu2.src = "assets/images/lampu_off.png"
     }
+
+    if(toggle3.checked) {
+        lampu3.src = "assets/images/lampu_on.png" 
+    }else {
+        lampu3.src = "assets/images/lampu_off.png"
+    }
+
+   
+
 }
